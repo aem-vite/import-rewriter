8d51559ecbe1ec06e679b803da2af18be63bac3a
Chris Shaw <me@cshaw.xyz>
2021-06-13 00:06:47 +1000

    feat: resolve import entry path for AEM BREAKING CHANGE: Main entry points are now rewritten
    All instances of the main entry point that need to refer back to the AEM ClientLib are automatically rewritten

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,30 @@ export interface ImportRewriterOptions {
 
 const relativePathPattern = /([.]{1,2}\/)+/
 
+let mainEntryPath: string | null = null
+
+/**
+ * Determines whether to use the native AEM ClientLib path or the provided `path`.
+ *
+ * @param path the import path
+ * @returns the real AEM ClientLib path or the original `path`
+ */
+function getAEMImportFilePath(path: string): string {
+  if (mainEntryPath && mainEntryPath === path) {
+    return `${path.substr(0, path.indexOf('/'))}.js`
+  }
+
+  return path
+}
+
+/**
+ * Converts the standard import path into an AEM compliant ClientLib path.
+ *
+ * @param path the import path
+ * @param options import rewriter options
+ * @param imports a list of available imports
+ * @returns an AEM compliant ClientLib path
+ */
 function getReplacementPath(path: string, options: ImportRewriterOptions, imports: string[]): string {
   if (options.command === 'serve') {
     return '/'
@@ -29,22 +53,29 @@ function getReplacementPath(path: string, options: ImportRewriterOptions, import
   return matchedImport
     ? path.replace(
         new RegExp(`${relativePathPattern.source}${path.replace(relativePathPattern, '')}`),
-        options.publicPath + matchedImport,
+        options.publicPath + getAEMImportFilePath(matchedImport),
       )
     : path
 }
 
+/**
+ * Identifies all ES imports and converts them into AEM ClientLib compliant paths.
+ *
+ * @param options import rewriter options
+ */
 export default function aemViteImportRewriter(options: ImportRewriterOptions): OutputPlugin {
   return {
     name: 'aem-vite:import-rewrite',
 
     async renderChunk(source, chunk, rollupOptions) {
-      console.log(`Current command: ${options.command}`)
-
       if (!options.command || !options.publicPath || !options.publicPath.length) {
         this.error(
           `Either 'command' or 'publicPath' haven't been defined, see https://aemvite.dev/guide/faqs/#vite-errors for more information.`,
         )
+      }
+
+      if (chunk.isEntry && chunk.facadeModuleId && /(ts|js)$/.test(chunk.facadeModuleId) && !mainEntryPath) {
+        mainEntryPath = chunk.fileName
       }
 
       await init
