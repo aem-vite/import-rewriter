94802832e9481869d68bbaba6bb6b0040bb2d253
Chris Shaw <me@cshaw.xyz>
2021-12-29 16:52:22 +1100

    refactor: cleaned up structure

---
+++ src/bundles.ts
@@ -0,0 +1,118 @@
+import { writeFileSync } from 'fs'
+import { join } from 'path'
+
+import { init, parse as parseImports } from 'es-module-lexer'
+import MagicString from 'magic-string'
+
+import {
+  getAEMImportFilePath,
+  getMainEntryPath,
+  getReplacementPath,
+  hasMainEntryPath,
+  isOutputChunk,
+  relativePathPattern,
+  setMainEntryPath,
+} from './helpers'
+
+import type { ImportSpecifier } from 'es-module-lexer'
+import type { NormalizedOutputOptions } from 'rollup'
+import type { Plugin } from 'vite'
+
+import type { BundlesImportRewriterOptions } from './types'
+
+/**
+ * Identifies all ES imports and converts them into AEM ClientLib compliant paths.
+ *
+ * @param options import rewriter options
+ */
+export function bundlesImportRewriter(options: BundlesImportRewriterOptions): Plugin {
+  return {
+    apply: 'build',
+    enforce: 'post',
+    name: 'aem-vite:import-rewriter',
+
+    async renderChunk(source, chunk, rollupOptions) {
+      if (rollupOptions.format !== 'es') {
+        return null
+      }
+
+      if (!options.publicPath || !options.publicPath.length) {
+        this.error(
+          `'publicPath' doesn't appear to be defined, see https://aemvite.dev/guide/faqs/#vite-errors for more information.`,
+        )
+      }
+
+      if (
+        options.mainEntryPath ||
+        (chunk.isEntry && chunk.facadeModuleId && /(ts|js)x?$/.test(chunk.facadeModuleId) && !hasMainEntryPath())
+      ) {
+        setMainEntryPath(options.mainEntryPath || chunk.fileName)
+      }
+
+      await init
+
+      let imports: ReadonlyArray<ImportSpecifier> = []
+      try {
+        imports = parseImports(source)[0]
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (e: any) {
+        this.error(e, e.idx)
+      }
+
+      if (!imports.length) {
+        return null
+      }
+
+      let s!: MagicString
+      const str = () => s || (s = new MagicString(source))
+
+      for (let index = 0; index < imports.length; index++) {
+        const { e: end, d: dynamicIndex, n: importPath, s: start } = imports[index]
+
+        if (dynamicIndex === -1 && importPath && relativePathPattern.test(importPath)) {
+          str().overwrite(start, end, getReplacementPath(importPath, options, chunk.imports))
+        }
+      }
+
+      if (s) {
+        return {
+          code: s.toString(),
+          map: rollupOptions.sourcemap !== false ? s.generateMap({ hires: true }) : null,
+        }
+      }
+
+      return null
+    },
+
+    writeBundle(rollupOptions, bundles) {
+      const mainEntryPath = getMainEntryPath()
+
+      for (const [fileName, chunkOrAsset] of Object.entries(bundles)) {
+        if (
+          !options.caching ||
+          !options.caching.enabled ||
+          !mainEntryPath ||
+          mainEntryPath === fileName ||
+          !isOutputChunk(chunkOrAsset) ||
+          !chunkOrAsset.code
+        ) {
+          continue
+        }
+
+        const mainEntryAEMPath = getAEMImportFilePath(mainEntryPath, options)
+
+        const mainEntryAEMPathWithHash = getAEMImportFilePath(
+          mainEntryPath,
+          options,
+          true,
+          rollupOptions as NormalizedOutputOptions,
+        )
+
+        writeFileSync(
+          join(rollupOptions.dir as string, fileName),
+          chunkOrAsset.code.replace(mainEntryAEMPath, mainEntryAEMPathWithHash),
+        )
+      }
+    },
+  }
+}
---
+++ src/css.ts
@@ -0,0 +1,32 @@
+import { createFilter } from 'rollup-pluginutils'
+
+import type { Plugin } from 'vite'
+
+import type { CssImportRewriterOptions } from './types'
+
+/**
+ * Identifies all static asset paths and converts them into AEM ClientLib compliant paths.
+ *
+ * @param options import rewriter options
+ */
+export function cssImportRewriter(options: CssImportRewriterOptions): Plugin {
+  const filter = createFilter(/\.(scss|sass|less|styl|stylus|css)$/i, [])
+
+  return {
+    enforce: 'post',
+    name: 'aem-vite:import-rewriter:css',
+
+    transform(code, id) {
+      if (filter(id)) {
+        code = code.replace(new RegExp(options.assetsBasePath, 'g'), options.publicPath)
+      }
+
+      return {
+        code,
+        id,
+        // TODO: Inherited from Vite's TODO to implement source maps
+        map: { mappings: '' },
+      }
+    },
+  }
+}
