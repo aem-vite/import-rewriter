539629dc89f97c37a98636bed41047ad75bdab55
Chris Shaw <me@cshaw.xyz>
2024-02-26 00:58:02 +1100

    test: prepare for coverage badge

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,9 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: ['cobertura', 'text'],
+      reporter: ['cobertura', 'json', 'json-summary', 'text'],
       reportsDirectory: '../coverage',
+      reportOnFailure: true,
     },
 
     reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
